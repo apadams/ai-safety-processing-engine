972711d573e8adddda49a3cea7bbd9a429f9e816
apadams <andpadam1@gmail.com>
2026-01-09 07:31:59 -0500

    Updated Allow list to fix reported issue.

--- filter/allowlist.go
+++ filter/allowlist.go
@@ -1,13 +1,14 @@
 package filter
 
 var Allowlist = map[string]bool{
-	"google.com":    true,
-	"openai.com":    true,
-	"microsoft.com": true,
-	"github.com":    true,
-	"amazon.com":    true,
-	"meta.com":      true,
-	"anthropic.com": true,
-    "notion.so":     true,
-    "linear.app":    true,
+	"google.com":     true,
+	"openai.com":     true,
+	"microsoft.com":  true,
+	"github.com":     true,
+	"amazon.com":     true,
+	"meta.com":       true,
+	"anthropic.com":  true,
+	"notion.so":      true,
+	"linear.app":     true,
+	"gruntless.work": true,
 }
